46870ba842e16a253867188923799f28545070bd
alanaynbund <aynbunda@gmail.com>
2023-05-27 17:17:03 -0400

    homepage chart done

---
+++ src/dummyData.js
@@ -0,0 +1,51 @@
+export const data = [
+    {
+      name: 'Jan',
+      sales: 4000
+    },
+    {
+      name: 'Feb',
+      sales: 3000
+    },
+    {
+      name: 'Mar',
+      sales: 2000
+    },
+    {
+      name: 'Apr',
+      sales: 5000
+    },
+    {
+      name: 'May',
+      sales: 2000
+    },
+    {
+      name: 'Jun',
+      sales: 6000
+    },
+    {
+      name: 'Jul',
+      sales: 8000
+    },
+    {
+      name: 'Aug',
+      sales: 5000
+    },
+    {
+      name: 'Sep',
+      sales: 3000
+    },
+    {
+      name: 'Oct',
+      sales: 6000
+    },
+    {
+      name: 'Nov',
+      sales: 8000
+    },
+    {
+      name: 'Dec',
+      sales: 10000
+    },
+    
+  ];
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import "./home.css"
 import FeaturedInfo from '../../components/featuredInfo/FeaturedInfo'
+import Chart from '../../components/chart/Chart'
+import {data} from "../../dummyData"
 
 export default function Home() {
   return (
     <div className='home'>
     <FeaturedInfo/>
+    <Chart data={data} title="Sales Analytics" grid dataKey="sales"/>
     </div>
   )
 }
